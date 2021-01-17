bf9d08b950a3372deebcdc36f93a3ed003e2ab9f
hboisgibault <h.boisgibault@gmail.com>
2021-01-17 20:14:38 +0100

    Htaccess for fonts

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,9 +15,9 @@ class Cors
      * @return mixed
      */
     public function handle($request, Closure $next)
-{
-    return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
-}
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+    }
 }
